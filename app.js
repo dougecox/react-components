00efd165340eefa02183cc9377e67355a3e6a1fe
Hack Reactor Student <student@hackreactor.com>
2017-04-12 14:45:33 -0700

    hi doug

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['p', 'p']}/> // Here we are creating an instance of the `TodoList` component
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
